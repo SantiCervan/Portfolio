b2d4c062f57fc9179a6eff45cc4d86e0c2347e82
s.cervan <santi.cervan.12@gmail.com>
2023-07-10 18:19:19 -0300

    Feat: mensaje cuando no esta en foco la web

--- components/index.tsx
+++ components/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect } from 'react';
 import NavBar from './NavBar';
 import Perfil from './Perfil';
 import Head from 'next/head';
 
 function index() {
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      document.title = document.hidden
+        ? 'SC - ¡No te pierdas mi trabajo! '
+        : 'Santiago Cervan - Front end';
+    };
+
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, []);
+
   return (
     <>
       <Head>
